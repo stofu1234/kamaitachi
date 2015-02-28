b09d2d76fc327afe724f2f52b133b24f228a4797
stofu1234 <yamaboku@gmail.com>
2015-02-28 17:15:41 +0900

    Initialize repository

---
+++ src/jp/co/stofu/kamaitachi/Awaiter.java
@@ -0,0 +1,133 @@
+package jp.co.stofu.kamaitachi;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Awaiter {
+
+	LinkedBlockingQueue<Runnable> waitingTaskQueue = new LinkedBlockingQueue<Runnable>();
+	Function<Exception, Void> errorHandler = null;
+	private boolean isDispatchThread = true;
+	private Thread dispatchThread = null;
+
+	public <T, R> BlockingQueue<R> await(Callable<T> heavyWorkTask,
+			Function<T, R> afterHeavyWorkTask) {
+		return await(heavyWorkTask, afterHeavyWorkTask, this.errorHandler);
+
+	}
+
+	public <T, R> BlockingQueue<R> await(Callable<T> heavyWorkTask,
+			Function<T, R> afterHeavyWorkTask,
+			Function<Exception, Void> errorHandler) {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		LinkedBlockingQueue<R> resultQueue = new LinkedBlockingQueue<R>();
+		// lambda式のタスクの中
+		// 1. heavyWorkTaskを実行し、型TのheavyWorkResultを受け取る
+		// 2. afterHeavyWorkTaskをheavyWorkResultとresultQueueを引数にカリー化でwaitingTaskにする
+		// 3. heavyWorkResultをresultQueueにaddする
+		// 4. waitingTaskをwaitingTaskQueueにaddする
+		executor.submit(() -> {
+			// heavyWorkControlTaskを非同期で実行
+			try {
+				final T heavyWorkResult = heavyWorkTask.call();
+				waitingTaskQueue.add(() -> {
+					R afterHeavyWorkResult = afterHeavyWorkTask
+							.apply(heavyWorkResult);
+					resultQueue.add(afterHeavyWorkResult);
+				});
+
+			} catch (Exception e) {
+				if (errorHandler == null) {
+					e.printStackTrace();
+				} else {
+					errorHandler.apply(e);
+				}
+			}
+
+		});
+		return resultQueue;
+	}
+
+	public <T> void awaitVoid(Callable<T> heavyWorkTask,
+			Consumer<T> afterHeavyWorkTask) {
+		awaitVoid(heavyWorkTask, afterHeavyWorkTask, this.errorHandler);
+	}
+
+	public <T> void awaitVoid(Callable<T> heavyWorkTask,
+			Consumer<T> afterHeavyWorkTask,
+			Function<Exception, Void> errorHandler) {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		// lambda式のタスクの中
+		// 1. heavyWorkTaskを実行し、型TのheavyWorkResultを受け取る
+		// 2.
+		// afterHeavyWorkTaskをheavyWorkResultとresultQueueを引数にカリー化でwaitingTaskにする
+		// 3. heavyWorkResultをresultQueueにaddする
+		// 4. waitingTaskをwaitingTaskQueueにaddする
+		executor.submit(() -> {
+			// heavyWorkControlTaskを非同期で実行
+			try {
+				final T heavyWorkResult = heavyWorkTask.call();
+				waitingTaskQueue.add(() -> {
+					afterHeavyWorkTask.accept(heavyWorkResult);
+				});
+
+			} catch (Exception e) {
+				if (errorHandler == null) {
+					e.printStackTrace();
+				} else {
+					errorHandler.apply(e);
+				}
+			}
+
+		});
+	}
+
+	public void dispatch() {
+		Runnable task = null;
+		while ((task = waitingTaskQueue.poll()) != null) {
+			task.run();
+		}
+	}
+
+	public void dispatchAsync() {
+		dispatchThread = new Thread(() -> {
+			Runnable task = null;
+			try {
+				while (isDispatchThread) {
+					task = waitingTaskQueue.take();
+					task.run();
+				}
+			} catch (InterruptedException e) {
+
+			}
+		});
+		dispatchThread.start();
+	}
+
+	public void stopDispatchThread() {
+		isDispatchThread = false;
+		waitingTaskQueue.add(() -> {
+		});
+	}
+
+	public void setErrorHandle(Function<Exception, Void> errorHandle) {
+		this.errorHandler = errorHandle;
+	}
+
+	public void clearErrorHandle() {
+		this.errorHandler = null;
+	}
+
+	Function<Exception, Void> getErrorHandle() {
+		return this.errorHandler;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/jp/co/stofu/kamaitachi/SwingAwaiterTester.java
@@ -0,0 +1,204 @@
+package jp.co.stofu.kamaitachi;
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SwingAwaiterTester extends JFrame {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -5885035566423727127L;
+
+	Timer mainTimer;
+	Timer neighborTimer;
+	Timer dispatchTimer;
+	JLabel label;
+	int mainSec;
+	int neighborSec;
+	Awaiter awaiter;
+	Thread mainThread;
+
+	SwingAwaiterTester() {
+		label = new JLabel();
+
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(label);
+		label.setText("dummy");
+		getContentPane().add(labelPanel, BorderLayout.CENTER);
+		awaiter = new Awaiter();
+
+		init();
+	}
+	public void init(){
+		mainSec = 0;
+		neighborSec = 0;
+
+	}
+
+	public synchronized void sleep() {
+		try {
+			wait();
+		} catch (InterruptedException e) {
+			System.out.println("返事がない。ただの屍のようだ");
+		}
+	}
+
+	public String getTime() {
+		Date date = new Date(System.currentTimeMillis());
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		return df.format(date);
+	}
+
+	public String doHeavyWork(String input) {
+		System.out.println(getTime() + " doHeavyWork start. "
+				+ Thread.currentThread().getName() + " ");
+		doSomeWork(5000); // 5秒のwait
+		System.out.println(getTime() + " doHeavyWork done. "
+				+ Thread.currentThread().getName() + " ");
+		return input + "ratta!! ";
+	}
+
+	public void doSomeWork(int millsec) {
+		try {
+			Thread.sleep(millsec);
+		} catch (InterruptedException e) {
+			System.out.println("返事がない。ただの屍のようだ");
+		}
+	}
+
+	public synchronized void stopAllTimer() {
+		mainTimer.stop();
+		neighborTimer.stop();
+		dispatchTimer.stop();
+		this.notifyAll();
+
+	}
+
+	public void runSynchrounousStackTest() {
+		init();
+
+		ActionListener dispatchThread = e -> {
+			awaiter.dispatch();
+		};
+		ActionListener mainThread = e -> {
+			System.out.println(getTime() + " Main Thread start:"
+					+ Thread.currentThread().getName() + " " + mainSec
+					+ " sec ");
+
+			String heavyWorkResult = doHeavyWork("hoge");
+			System.out.println(getTime() + " afterHeavyWork start. "
+					+ Thread.currentThread().getName() + " " + heavyWorkResult);
+			if (mainSec >= 10) {
+				stopAllTimer();
+			} else {
+				this.mainSec++;
+			}
+			System.out.println(getTime() + " afterHeavyWork done. "
+					+ Thread.currentThread().getName() + " " + heavyWorkResult);
+
+			System.out.println(getTime() + " Main Thread done.:"
+					+ Thread.currentThread().getName() + " " + mainSec
+					+ " sec ");
+
+		};
+		ActionListener neighborThread = e -> {
+			System.out.println(getTime() + " Neighbor Thread start:"
+					+ Thread.currentThread().getName() + " " + neighborSec
+					+ " sec ");
+
+			neighborSec++;
+			System.out.println(getTime() + " Neighbor Thread done:"
+					+ Thread.currentThread().getName() + " " + neighborSec
+					+ " sec ");
+		};
+
+		mainTimer = new Timer(1000, mainThread);
+		mainTimer.start();
+		neighborTimer = new Timer(1000, neighborThread);
+		neighborTimer.start();
+		dispatchTimer = new Timer(1000, dispatchThread);
+		dispatchTimer.start();
+		sleep();
+	}
+
+	public void runASynchrounousStacklessTest() {
+		init();
+
+		ActionListener dispatchThread = e -> {
+			awaiter.dispatch();
+		};
+		ActionListener mainThread = e -> {
+			System.out.println(getTime() + " Main Thread start:"
+					+ Thread.currentThread().getName() + " " + mainSec
+					+ " sec ");
+
+			awaiter.awaitVoid(
+					() -> doHeavyWork("hoge"),
+					heavyWorkResult -> {
+						System.out.println(getTime()
+								+ " afterHeavyWork start. "
+								+ Thread.currentThread().getName() + " "
+								+ heavyWorkResult);
+						if (mainSec >= 10) {
+							// this.mainTimer.stop();
+							stopAllTimer();
+						} else {
+							this.mainSec++;
+						}
+						System.out.println(getTime() + " afterHeavyWork done. "
+								+ Thread.currentThread().getName() + " "
+								+ heavyWorkResult);
+
+					});
+			System.out.println(getTime() + " Main Thread done.:"
+					+ Thread.currentThread().getName() + " " + mainSec
+					+ " sec ");
+
+		};
+		ActionListener neighborThread = e -> {
+			System.out.println(getTime() + " Neighbor Thread start:"
+					+ Thread.currentThread().getName() + " " + neighborSec
+					+ " sec ");
+
+			neighborSec++;
+			System.out.println(getTime() + " Neighbor Thread done:"
+					+ Thread.currentThread().getName() + " " + neighborSec
+					+ " sec ");
+		};
+
+		mainTimer = new Timer(1000, mainThread);
+		mainTimer.start();
+		neighborTimer = new Timer(1000, neighborThread);
+		neighborTimer.start();
+		dispatchTimer = new Timer(1000, dispatchThread);
+		dispatchTimer.start();
+		sleep();
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("main start!");
+		SwingAwaiterTester frame = new SwingAwaiterTester();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(10, 10, 250, 100);
+		frame.setTitle("Dummy Window");
+		frame.setVisible(true);
+
+
+		System.out.println("synchronous stack test start!");
+		frame.runSynchrounousStackTest();
+		System.out.println("asynchronous stackless test start!");
+		frame.runASynchrounousStacklessTest();
+
+		System.out.println("main end!");
+	}
+
+}
